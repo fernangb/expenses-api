2e18310171d6713a547abd3223c89c77365be839
Gabriel Fernandes de Souza <gfsouza@poli.ufrj.br>
2022-06-27 23:27:41 -0300

    docs: use env variables

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+<p align="center">API to manage expenses</p>
+<p align="center">
+  To test my endpoints, please check
+  <a href="http://localhost:3000/docs" target="blank"></a>
+</p>
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo_text.svg" width="320" alt="Nest Logo" /></a>
 </p>
--- src/database.module.ts
+++ src/database.module.ts
@@ -8,11 +8,11 @@ import { join } from 'path';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '',
-      port: null,
-      username: '',
-      password: '',
-      database: '',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [
         join(
           __dirname,
